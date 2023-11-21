ab39c457576e7ef5d22c1e792b88cefd8879ebee
yunquanzi <50273955+yunquanzi@users.noreply.github.com>
2023-11-21 21:12:58 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://02035.vip/tv/");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
