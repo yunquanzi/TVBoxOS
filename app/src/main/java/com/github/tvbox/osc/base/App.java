784a347af29dcd8405de573a8a32cb4b59ddc2bf
yunquanzi <50273955+yunquanzi@users.noreply.github.com>
2023-11-23 02:07:46 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -66,13 +66,10 @@ public class App extends MultiDexApplication {
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
-    putDefault(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
-    putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
-    putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
-    putDefault(HawkConfig.HOME_SHOW_SOURCE, true);  // true=Show, false=Not show
-    putDefault(HawkConfig.HOME_NUM, 2);       // History Number
-    putDefault(HawkConfig.DOH_URL, 2);        // DNS
-    putDefault(HawkConfig.SEARCH_VIEW, 2);    // Text or Picture
+    if (!Hawk.contains(HawkConfig.IJK_CODEC)) { Hawk.put(HawkConfig.IJK_CODEC, “硬解码”); }
+    if (!Hawk.contains(HawkConfig.DOH_URL)) { Hawk.put(HawkConfig.DOH_URL,2); }
+    if (!Hawk.contains(HawkConfig.SEARCH_VIEW))
+       { Hawk.put(HawkConfig.SEARCH_VIEW, 2); }
         }
     }
 
