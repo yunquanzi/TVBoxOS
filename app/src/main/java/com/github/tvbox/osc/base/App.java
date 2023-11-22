00e43ae4c78b461f03cc6740968d540ba2e1ed3d
yunquanzi <50273955+yunquanzi@users.noreply.github.com>
2023-11-23 02:13:05 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -66,10 +66,6 @@ public class App extends MultiDexApplication {
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
-    if (!Hawk.contains(HawkConfig.IJK_CODEC)) { Hawk.put(HawkConfig.IJK_CODEC, “硬解码”); }
-    if (!Hawk.contains(HawkConfig.DOH_URL)) { Hawk.put(HawkConfig.DOH_URL,2); }
-    if (!Hawk.contains(HawkConfig.SEARCH_VIEW))
-       { Hawk.put(HawkConfig.SEARCH_VIEW, 2); }
         }
     }
 
